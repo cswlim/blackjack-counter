75efc3307ee7d17a768918e35f123f34ace99c87
cswlim <162635025+cswlim@users.noreply.github.com>
2025-07-26 17:19:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 server.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
